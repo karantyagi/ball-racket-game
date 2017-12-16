3701d300f1a1680b8cf6a692ea00458ee95afc2b
Hitesh <hitzver@gmail.com>
2017-12-16 16:34:13 -0500

    Racket.rkt

--- Racket.rkt
+++ Racket.rkt
@@ -35,7 +35,7 @@
 ;;; color of circle:  MOUSE-COLOR
 ;;; radius of circle: MOUSE-RADIUS
 
-(define MOUSE-RACKET-DISTANCE 25)
+(define MOUSE-RACKET-DISTANCE 20)
 ;;; The maximum distance of mouse from rectangle, to grab and select
 ;;; the rectangle.
 
@@ -62,14 +62,14 @@
 ;;;
 ;;; A Racket is represented as a struct (make-racket x y vx vy mouse selected?)
 ;;; with the following fields:
-;;; x  : Inte  x-coordinate of the center of the racket, in pixels, 
+;;; x  : Int  x-coordinate of the center of the racket, in pixels, 
 ;;;            relative to the origin of the scene, assuming the racket
 ;;;            to be a straight line of a fixed length
 ;;; y  : Int   y-coordinate of the center of the racket, in pixels,
 ;;;            relative to the origin of the scene
 ;;; vx : Int   x component of the racket's velocity, in pixels/tick
 ;;; vy : Int   y component of the racket's velocity, in pixels/tick
-;;; mouse : Mouse  describes the coordinates of mouse on grabbing and 
+;;; mx,my:Int  describes the relative coordinates of mouse on grabbing and 
 ;;;                selecting the racket for smooth dragging
 ;;; selected? : Boolean   true iff the racket is selected
 
@@ -92,3 +92,120 @@
 
 
 
+<<<<<<< HEAD
+=======
+;;; IMPLEMENTATION:
+(define-struct mouse (x y))
+
+;;; CONSTRUCTOR TEMPLATE
+;;; (make-mouse Integer Integer)
+
+;;; OBSERVER TEMPLATE
+;;; mouse-fn : Mouse -> ??
+(define (mouse-fn m)
+  (... (mouse-x m)
+       (mouse-y m)))
+
+
+
+;;;Functions 
+
+;;; racket-after-mouse-event : Racket Int Int MouseEvent -> Racket
+;; GIVEN: a racket, the x and y coordinates of a mouse event,
+;;     and the mouse event
+;; RETURNS: the racket as it should be after the given mouse event
+;; EXAMPLES:
+;; (racket-after-mouse-event
+;;                        (make-racket 330 384 0 0 0 0 #false)
+;;                           320
+;;                            390
+;;                            "button-down")
+;;                -->   (make-racket 330 384 0 0 320 390 #t)
+;; strategy: Cases on mouse event mev
+(define (racket-after-mouse-event r mx my mev)
+  (cond
+    [(mouse=? mev "button-down") (racket-after-button-down r mx my)]
+    [(mouse=? mev "drag") (racket-after-drag r mx my)]
+    [(mouse=? mev "button-up") (racket-after-button-up r mx my)]
+    [else r]))
+
+
+
+;; racket-after-button-down :Racket  Integer Integer -> Racket
+;; GIVEN: racket r , mouse x pos , mouse y pos
+;; RETURNS: the racket following a button-down at the given location
+;; EXAMPLES:
+;;(racket-after-button-down
+;;    (make-racket 330 384 0 0 0 0 #false) 320 390)
+;; ->   (make-racket 330 384 0 0 320 390 #t)
+
+;; STRATEGY: divide into cases on r,x and y 
+
+(define (racket-after-button-down r x y)
+  (if (in-racket? r x y)
+      (make-racket (racket-x r) (racket-y r)
+                   (racket-vx r) (racket-vy r)
+                   x y
+                   #true)r))
+
+;; racket-after-drag : Racket Integer Integer -> Racket
+;; GIVEN: racket r , mouse x pos , mouse y pos
+;; RETURNS: the racket following a drag at the given location
+;; EXAMPLES:
+;; (racket-after-drag
+;;                           (make-racket 330 384 0 0 #true)
+;;                           320
+;;                           390)
+;;                          -> (make-racket 320 390 0 0 320 390 #true)
+;; STRATEGY: Use template for Racket on r
+(define (racket-after-drag r x y)
+  (if (racket-selected? r) (make-racket
+                            (- (racket-x r) (- (racket-mx r) x))
+                            (- (racket-y r) (- (racket-my r) y))
+                            (racket-vx r)
+                            (racket-vy r)
+                            x y true)
+      r))
+
+
+;; racket-after-button-up : Racket Integer Integer -> Racket
+;; GIVEN: Racket r , mouse x pos , mouse y pos
+;; RETURNS: the racket following a button-up at the given location
+;; EXAMPLES :
+;;racket-selected? (racket-after-button-up
+;;                          (make-racket 330 384 0 0 0 0 #true)
+;;                           330
+;;                           380))
+;;                     ->  #f
+;; DESIGN STRATEGY: Use template for racket on r
+
+(define (racket-after-button-up r x y)
+  (make-racket (racket-x r) (racket-y r)
+               (racket-vx r) (racket-vy r) 0 0 #false))
+
+;; in-racket? : Racket Integer Integer -> Racket
+;; GIVEN: Racket r , mouse x pos , mouse y pos
+;; RETURNS true iff the given coordinate is inside the bounding region of
+;; the given racket.
+;; EXAMPLES:
+;;(in-racket?
+;;        (make-racket 330 384 0 0 0 0 #true)
+;;           330
+;;            384)
+;;  --->  #t
+;; DESIGN STRATEGY: Use observer template for Racket on r
+
+(define (in-racket? r x y)
+  (and
+   (<= (- (racket-x r) MOUSE-RACKET-DISTANCE)
+       x
+       (+ (racket-x r) MOUSE-RACKET-DISTANCE))
+   (<= 
+    (- (racket-y r) MOUSE-RACKET-DISTANCE)
+    y
+    (+ (racket-y r) MOUSE-RACKET-DISTANCE))))
+
+
+
+
+>>>>>>> updated racket,Render-game,world
