1c454a553369071ff5147081578ad8df999535c1
Hitesh <hitzver@gmail.com>
2017-12-18 01:42:40 -0500

    updated balls.rkt

--- Balls.rkt
+++ Balls.rkt
@@ -7,6 +7,7 @@
 (require "extras.rkt") 
 (require 2htdp/universe)
 (require 2htdp/image)
+(require "Racket.rkt")
 
 (provide
  BALL-COLOR
@@ -20,14 +21,25 @@
  ball-x
  ball-y
  ball-vx
- ball-vy)
+ ball-vy
+ ball-next-move
+ ball-collide-wall?
+ ball-collide-topwall?
+ ball-collide-rightwall?
+ any-ball-collide-racket?
+ after-ball-collide-wall
+ ball-collide-racket?
+ ball-after-colliding-racket
+ after-ball-collide-topwall
+ after-ball-collide-leftwall
+ after-ball-collide-rightwall)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;;; CONSTANTS
 
 (define BALL-COLOR "black")
-(define BALL-RADIUS 6)
+(define BALL-RADIUS 10)
 ;;; Ball is rendered as a circle of color: BALL-COLOR
 ;;; and radius: BALL-RADIUS
 
@@ -39,6 +51,11 @@
 (define INITIAL-BALL-VX 20)
 (define INITIAL-BALL-VY 20)
 ;;; initial velocity of ball in ready-to-serve state
+
+(define HALF-LENGTH 47/2) ;; racket half length
+
+(define COURT-WIDTH 940)
+(define COURT-HEIGHT 680)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; DATA DEFINITION
@@ -100,3 +117,89 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; EXAMPLES OF WORLDS FOR TESTING:
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (ball-collide-wall? b)
+  ( or (ball-collide-topwall? b) (ball-collide-leftwall? b)
+       (ball-collide-rightwall? b)))
+
+
+(define (ball-collide-topwall? b)
+  (< ( + (ball-y b) (ball-vy b)) 0))
+
+(define (ball-collide-leftwall? b)
+  (< (+ (ball-x b) (ball-vx b)) 0))
+
+(define (ball-collide-rightwall? b)
+  (< COURT-WIDTH (+ (ball-x b) (ball-vx b))))
+
+
+(define (any-ball-collide-racket? bl r)
+  (ormap
+   ;Ball -> Boolean
+   ;GIVEN: a ball
+   ;RETURNS: true iff given ball collides racket
+   (lambda (b) (ball-collide-racket? b r))
+   bl))
+
+(define (after-ball-collide-wall b)
+  (cond
+    [(ball-collide-topwall? b) (after-ball-collide-topwall b)]
+    [(ball-collide-leftwall? b) (after-ball-collide-leftwall b)]
+    [(ball-collide-rightwall? b) (after-ball-collide-rightwall b)]
+    ))
+
+
+(define (ball-collide-racket? b r)
+  ( collision-path?  (ball-x b)
+                     (ball-y b)
+                     (ball-vx b)
+                     (ball-vy b)
+                     (+ (racket-x r) (racket-vx r))
+                     (+ (racket-y r) (racket-vy r))))
+
+
+(define (collision-path? bx by bvx bvy rx ry )
+  ( and (x-range? bx (- rx HALF-LENGTH) (+ rx HALF-LENGTH))
+        (x-range? (+ bx bvx) (- rx HALF-LENGTH) (+ rx HALF-LENGTH))
+        (< by ry)
+        (<= ry (+ by bvy))))
+
+
+(define (x-range? bx1 rx1 rx2 )
+  ( and (< rx1 bx1 ) (< bx1 rx2)))
+
+
+(define (ball-after-colliding-racket b r)
+  (make-ball (ball-x b)
+             (ball-y b)
+             (ball-vx b)
+             (- (racket-vy r) (ball-vy b))))
+
+
+(define (after-ball-collide-topwall b)
+  (make-ball (+ (ball-x b) (ball-vx b))
+             (- (+ (ball-y b) (ball-vy b)))
+             (ball-vx b)
+             (- (ball-vy b)) ))
+
+
+(define (after-ball-collide-leftwall b)
+  (make-ball (-(+ (ball-x b) (ball-vx b)))
+             (+ (ball-y b) (ball-vy b))
+             (- (ball-vx b)) (ball-vy b)))
+
+(define (after-ball-collide-rightwall b)
+  (make-ball (- COURT-WIDTH (-(+ (ball-x b) (ball-vx b)) COURT-WIDTH))
+             (+ (ball-y b) (ball-vy b))
+             (-(ball-vx b))
+             (ball-vy b) ))
+
+(define (ball-next-move b)
+  (make-ball
+   (+ (ball-x b) (ball-vx b))
+   (+ (ball-y b) (ball-vy b))
+   (ball-vx b)
+   (ball-vy b)))
+
+
+
